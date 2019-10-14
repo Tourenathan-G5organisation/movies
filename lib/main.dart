5f709851061c5997d4cf1a0733fa2c5471fdb24d
TOURE NATHAN <toure21@live.com>
2019-10-14 22:33:48 +0300

    Created fake data for movie object

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
+        accentColor: const Color(0xFFFF5959),
       ),
       home: HomePage(title: title),
     );
--- lib/ui/detailpage.dart
+++ lib/ui/detailpage.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:movies/model/Actor.dart';
+import 'package:movies/model/Movie.dart';
 
 class DetailPage extends StatefulWidget {
-
   DetailPage({Key key}) : super(key: key);
-
 
   @override
   _DetailPageState createState() => _DetailPageState();
-
 }
 
-class _DetailPageState extends State<DetailPage>{
+class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,4 +22,45 @@ class _DetailPageState extends State<DetailPage>{
     );
   }
 
+  Movie _createFakeData() {
+    return Movie(
+        bannerUrl: 'images/banner.png',
+        posterUrl: 'images/poster.png',
+        title: 'Le Blanc d\'Eyenga 2',
+        rating: 8.0,
+        starRating: 4,
+        categories: ['Comedy', 'Drame'],
+        storyline: 'For their fifth fully-animated feature-film '
+            'collaboration, Illumination Entertainment and Universal '
+            'Pictures present The Secret Life of Pets, a comedy about '
+            'the lives our...',
+        photoUrls: [
+          'images/image1.jpg',
+          'images/image2.jpg',
+          'images/image3.jpg',
+          'images/image4.jpg',
+        ],
+        actors: [
+          Actor(
+            name: 'Thierry Ntamack',
+            avatarUrl: 'images/tamack.jpg',
+          ),
+          Actor(
+            name: 'Aicha Wète Kaprisky',
+            avatarUrl: 'images/aicha.jpg',
+          ),
+          Actor(
+            name: 'Lucie Memba',
+            avatarUrl: 'images/lucie.jpg',
+          ),
+          Actor(
+            name: 'Kaline Truong',
+            avatarUrl: 'images/kaline.jpg',
+          ),
+          Actor(
+            name: 'Jeanne Mbenti',
+            avatarUrl: 'images/jeane.jpg',
+          )
+        ]);
+  }
 }
